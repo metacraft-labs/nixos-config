9abce5ced9e997e288a20bdfbb35f315969e6f19
Dimo99 <dimodimov1999@gmail.com>
2022-08-30 14:51:23 +0300

    hardware configuration changes

--- nixos/machines/mcl-dimo-nixos-desktop03/hardware-configuration.nix
+++ nixos/machines/mcl-dimo-nixos-desktop03/hardware-configuration.nix
@@ -1,11 +1,12 @@
 {lib, ...}: {
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = ["amd_pstate" "kvm-amd"];
   boot.kernelParams = [
     "initcall_blacklist=acpi_cpufreq_init"
   ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [];
+  services.xserver.videoDrivers = ["nvidia"];
 
   imports = [./file-systems.nix];
 
