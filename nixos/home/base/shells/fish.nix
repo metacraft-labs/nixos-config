3b489c5ed9516c17c159f15b2cf9ddde8c31b5c3
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2023-03-27 19:56:30 +0300

    config(fish): Add aliase for `tilix work session`

--- nixos/home/base/shells/fish.nix
+++ nixos/home/base/shells/fish.nix
@@ -165,6 +165,9 @@
 
       # xcpil aliases:
       copy = "xclip -selection clipboard ";
+
+      #tilix aliases:
+      work2 = "tilix --session $CODE/tilix_work2_session.json && exit";
     };
   };
 }
