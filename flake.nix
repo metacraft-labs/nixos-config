EmilIvanichkovv/.dotfiles

Update my configuration

flake.nix
@@ -32,6 +32,8 @@
     nix-on-droid,
     ...
   }: let
+    username = "Emil";
+    host = "emil-nixos-acer-nitro";
     system = "x86_64-linux";
     defaultUser = "zlx";
     users = [defaultUser];

metacraft-labs/nixos-config

flake.nix
@@ -38,7 +38,7 @@
     flake-parts,
     ...
   } @ inputs: let
-    defaultUser = "zlx";
+    defaultUser = "Emil";
     instantiateMachines = (import ./nixos/machines) {lib = nixpkgs.lib;};
   in
     flake-parts.lib.mkFlake {inherit inputs;} {