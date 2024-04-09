a65b47444572acf5cd0c0c3d3106490663cc2aba
Pavel Penev <pvl.penev@gmail.com>
2024-04-09 13:59:10 +0300

    changed keyboard layout combo

--- nixos/sys/kde_desktop_env.nix
+++ nixos/sys/kde_desktop_env.nix
@@ -5,7 +5,7 @@
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.layout = "us,bg";
   services.xserver.xkbVariant = "dvorak,";  
-  services.xserver.xkbOptions = "grp:lalt_lshift_toggle,altwin:ctrl_alt_win";
+  services.xserver.xkbOptions = "grp:shifts_toggle,altwin:ctrl_alt_win";
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   # Needed for Home Manager to be able to update DConf settings
