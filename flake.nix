MartinNikov/dotfiles

added mcl-nixos-desktop10

flake.nix
@@ -38,7 +38,7 @@
     flake-parts,
     ...
   } @ inputs: let
-    defaultUser = "zlx";
+    defaultUser = "zahary";
     instantiateMachines = (import ./nixos/machines) {lib = nixpkgs.lib;};
   in
     flake-parts.lib.mkFlake {inherit inputs;} {

metacraft-labs/nixos-config

flake.nix
@@ -58,7 +58,7 @@
     flake-parts,
     ...
   } @ inputs: let
-    defaultUser = "zlx";
+    defaultUser = "stefan";
   in
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];